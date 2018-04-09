547bc4045f3c34f4253911e137cf4288be46d11b
platrol <takialog@gmail.com>
2018-04-10 00:37:35 +0800

    Done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,24 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.params['sort'] = 'created'
+    end
+    @repos_list = JSON.parse(resp.body)
   end
 
   def create
+    resp =  Faraday.post('https://api.github.com/user/repos') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+      req.body = { name: params[:name]}.to_json
+    end
+    if resp.success?
+      redirect_to root_url
+    else
+      @error = JSON.parse(resp.body)
+      render :index
+    end
+    # byebug
+
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
+  skip_before_action  :authenticate_user, only: :create
+
   def create
+    resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
+      req.params['client_id'] = ENV['CLIENT_ID']
+      req.params['client_secret'] =  ENV['CLIENT_SECRET']
+      req.params['code'] = params['code']
+      # req.params['redirect_uri'] = "http://127.0.0.1:3000/auth"
+      req.params['state'] = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body['access_token']
+
+    user_resp = Faraday.get('https://api.github.com/user') do |req|
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+
+    user_json= JSON.parse(user_resp.body)
+    session[:username] = user_json['login']
+    # byebug
+    redirect_to '/'
+
   end
 end
