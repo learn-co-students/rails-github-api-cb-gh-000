64acb87deb7f1fdeff77773d4fd8b48bf567c977
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-07 11:52:08 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,16 @@
 class RepositoriesController < ApplicationController
+
   def index
+    github = GithubService.new(session[:token])
+    @repos_list, @page_link = github.get_repos(params[:page])
   end
 
   def create
+    github = GithubService.new(session[:token])
+    is_error = github.create_repos(params[:name])
+    # byebug
+    # render :index if @is_error
+    flash[:error] = is_error['message'] if is_error
+    redirect_to root_url
   end
 end
